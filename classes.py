5b53056995f8683719b8838e4d21ac4af42f87b6
Victor804 <victor.vaucel@gmail.com>
2018-09-29 16:15:09 +0200

    update

--- classes.py
+++ classes.py
@@ -2,55 +2,44 @@ import pygame
 from functions import *
 import math
 
-class Aim(pygame.sprite.Sprite):
-	def __init__(self):
-		super().__init__()
+class Aim:
+	def __init__(self, display):
+		self.display = display
 		self.image = pygame.transform.scale(load_file("./images/aim.png"), (percentPix(2, True), percentPix(2, False)))
-		self.rect = self.image.get_rect()
-		screen = pygame.display.get_surface()
-		self.area = screen.get_rect()
 
 	def focusAim(self):
-		Xmouse, Ymouse = pygame.mouse.get_pos()
-		self.rect.x = Xmouse
-		self.rect.y = Ymouse
+		self.display.blit(self.image, pygame.mouse.get_pos())
 
 
 
-class Bullet(pygame.sprite.Sprite):
-	def __init__(self, element, power):
-		super().__init__()
-		self.image = pygame.Surface((percentPix(2, True), percentPix(2, False)))
+class Bullet:
+	def __init__(self, display, x, y):
+		self.image = pygame.transform.scale(load_file("./images/bullet.jpg"), (percentPix(2, True), percentPix(2, False)))
 		self.rect = self.image.get_rect()
-		self.rect.y = element.rect.y
-		self.rect.x = element.rect.x
-		self.mouseX, self.mouseY = pygame.mouse.get_pos()
-		self.distanceY = self.mouseY - self.rect.y
-		self.distanceX = self.mouseX - self.rect.x
-		self.posXElement = element.rect.x
-		self.posYElement = element.rect.y
+		self.rect.x = x
+		self.rect.y = y
+		self.xMouse, self.yMouse = pygame.mouse.get_pos()
+		self.coof = (self.yMouse-self.rect.y)//(self.xMouse-self.rect.x)
 
-	def update(self, element):
-		self.rect.x = self.rect.x + 10
-		if 0 not in (self.distanceX, self.distanceY, self.posXElement, self.rect.x, self.posYElement, self.rect.y):
-			self.rect.y = self.posYElement - ((self.distanceY / self.distanceX ) *  (self.posXElement - self.rect.x ))
+	def update(self):
+		self.rect.x +=10
+		self.rect.y =  self.coof * self.rect.x
 
 
-
-class Player(pygame.sprite.Sprite):
-	def __init__(self):
-		super().__init__()
+class Player:
+	def __init__(self, display):
+		self.display = display
 		self.image = pygame.transform.scale(load_file("./images/spaceShip.png"), (percentPix(8, True), percentPix(10, False)))
 		self.rect = self.image.get_rect()
 		self.width, self.height = self.rect[-2::]
 		self.widthDisplay, self.heightDisplay = pygame.display.get_surface().get_size()
+		self.bullet = Bullet
+		self.list_bullets = []
 		self.alive = True
 		self.speed = 10
 
 	def move(self, posx, posy):
 		self.widthDisplay, self.heightDisplay = pygame.display.get_surface().get_size()
-		print(self.heightDisplay, self.height)
-		print(self.rect.y)
 		if posy == 1 and self.rect.y <= self.heightDisplay-self.height:
 			self.rect.y += self.speed
 		elif posy == -1 and self.rect.y > 0:
@@ -59,6 +48,16 @@ class Player(pygame.sprite.Sprite):
 			self.rect.x += self.speed
 		elif posx == -1 and self.rect.x > 0 :
 			self.rect.x -= self.speed
+
+	def shoot(self):
+		self.list_bullets.append(self.bullet(self.display, self.rect.x, self.rect.y))
+
+	def update(self):
+		for bullet in self.list_bullets:
+			bullet.update()
+			self.display.blit(bullet.image, (bullet.rect.x, bullet.rect.y))
+
+		self.display.blit(self.image, (self.rect.x, self.rect.y))
 
 
 
--- main.py
+++ main.py
@@ -3,43 +3,34 @@ from classes import *
 from functions import *
 
 #ignite the window with title and size
+displaySize = (1920, 1200)
 pygame.init()
-window = pygame.display.set_mode((1920, 1020), pygame.RESIZABLE)
+display = pygame.display.set_mode(displaySize, pygame.RESIZABLE)
 pygame.display.set_caption("Space Invaders")
 
 #load pics and resize it, load clock, load player and aim
 icon = load_file("./images/spaceInvaders_icon.jpg")
 background = load_file("./images/background.png")
-background = pygame.transform.scale(background, (1920, 1200))
+background = pygame.transform.scale(background, displaySize)
 
 pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 
-aim = Aim()
-player = Player()
-allsprites = pygame.sprite.RenderPlain((player, aim))
-bullet_list = pygame.sprite.Group()
-enemy_list = pygame.sprite.Group()
+aim = Aim(display)
+player = Player(display)
 pygame.mouse.set_visible(False)
 PAUSE = 0
 RUNNING = 1
 state = RUNNING
-pauseSurf = pygame.Surface(pygame.display.get_surface().get_size())
-pauseSurf.fill(pygame.Color("black"))
-pauseSurf.set_alpha(0)
 
 #start the window loop
 loop = True
 while loop:
 	#the play part
 	if state == RUNNING:
-		clock.tick(30)
-		window.blit(background, (0,0))
-		window.blit(pauseSurf, (0,0))
-		allsprites.draw(window)
-		pygame.event.pump()
+		clock.tick(60)
+		display.blit(background, (0,0))
 		aim.focusAim()
-
 
 		keypress = pygame.key.get_pressed()
 		if keypress[pygame.K_z]:
@@ -55,27 +46,11 @@ while loop:
 			if event.type == pygame.QUIT:
 				loop = False
 			if event.type == pygame.MOUSEBUTTONDOWN:
-				bullet = Bullet(player, 7)
-				allsprites.add(bullet)
-				bullet_list.add(bullet)
+				player.shoot()
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_ESCAPE: state = PAUSE
-		bullet_list.update(player)
 
-	#the pause part
-	if state == PAUSE:
-		pauseSurf.set_alpha(255)
-		pauseLoop = True
-		for eventPause in pygame.event.get():
-			if eventPause.type == pygame.KEYDOWN:
-				if eventPause.key == pygame.K_ESCAPE:
-					pauseSurf.set_alpha(0)
-					state = RUNNING
-			if event.type == pygame.QUIT:
-				loop = False
-
-
-
+	player.update()
 	pygame.display.flip()
 
 pygame.quit()
